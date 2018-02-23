8f405f7358c0760fa02d641715929a273d1cad16
junyung1261 <junyung1261@naver.com>
2018-02-23 20:30:12 +0900

    tt

--- src/pages/contest/contest.ts
+++ src/pages/contest/contest.ts
@@ -16,7 +16,6 @@ export class ContestPage {
     showBackdrop: true,
     enableBackdropDismiss: true,
     cssClass:'mini-modal'
-<<<<<<< HEAD
   }
 
   private numOfMale: any;
@@ -79,72 +78,6 @@ export class ContestPage {
 
   }
 
-=======
-  }
-
-  private contestProgress: any;
-
-  private numOfMale: any;
-  private numOfFemale: any;
-  private participents: any;
-  // private participentKey :any = {nickname : ""};
-  private participentKey = [];
-  // private participentInfo = [];
-  private participentInfo: any;
-
-  private pageFlag: boolean = false;
-
-
-
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, public afDB: AngularFireDatabase, public loadingCtrl: LoadingController) {
-
-    // let loading = this.loadingCtrl.create({
-    //   content: 'Fuck you...'
-    // });
-    // console.log("constructor")
-    // loading.present();
-    // setTimeout(() => {
-    //   loading.dismiss();
-    //   this.pageFlag = true;
-    // }, 3000);
-
-    var index = 0;
-    this.afDB.list('/contests/contestPK/female', ref=>ref.orderByKey()).snapshotChanges().take(1).subscribe(participents => {
-      this.participents = participents;
-      this.numOfFemale = participents.length;
-      // html에 쓸 때는 participents.payload.val().name 이딴식으로 쓰면 됨.
-      console.log("여성 참가자 수 : ", this.numOfFemale);
-
-
-      participents.forEach(participent => {
-
-        this.participentKey[index] = participent.key;
-        console.log("index 값 : ", index, "key 값 : ", this.participentKey[index])
-
-        // this.angularfireDatabase.list('/feed', ref => lastKey?  ref.orderByKey().limitToLast(batch).endAt(lastKey) : ref.orderByKey().limitToLast(batch));
-
-        // this.afDB.database.ref('/accounts/' + this.participentKey[index]).on('value', (snapshot) => {
-        //   this.participentInfo[index] = snapshot.child('username').val();
-        //   console.log("index 값 : ", index, "닉네임 값 : ", this.participentInfo[index])
-        // });
-
-        index = index + 1;
-
-      })
-
-    });
-
-    this.afDB.list('/accounts', ref => ref.orderByChild('contestApply').equalTo(true)).snapshotChanges().take(1).subscribe(participentsInfo => {
-      this.participentInfo = participentsInfo;
-      console.log(this.participentInfo)
-    })
-
-
-
-  }
-
->>>>>>> 08b45c161b259f600e6fdba9996aff28dc6f5c1c
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MatchingPage');
